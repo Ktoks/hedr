5a97134832413a69f1c1de61c8e39f944ed4e38e
Ktoks <kstocks0811@gmail.com>
2024-03-23 17:15:22 -0600

    Minimum viable Rust head command replacement

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(name = "hedr")]
+#[command(about = "Print the first 10 lines of each FILE to standard output.\nWith more than one FILE, precede each with a header giving the file name.\n\nWith no FILE, or when FILE is -, read standard input.")]
+#[command(long_about = None)]
+#[command(version = ".1")]
+struct Args {
+    fname: String,
+
+    #[arg(short, long, default_value_t = 10)]
+    num_of_lines: u8,
+
+    #[arg(short, long, default_value_t = 0)]
+    character_length: u8,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let mut reader = BufReader::new(File::open(args.fname).expect("open failed"));
+
+    if args.character_length > 0 {
+        let mut iterator = 0;
+        for line in reader.lines() {
+            for car in line.expect("lines failed").chars() {
+                if iterator >= args.character_length {
+                    break;
+                }
+                print!("{}", car);
+                iterator+=1;
+            }
+        }
+    }
+    else {
+        for _ in 0..args.num_of_lines {
+            let mut line = String::new();
+            reader.read_line(&mut line).unwrap();
+            println!("{}", line);
+        }
+    }
+}
